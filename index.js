2d8f9dc9d2c47ee28c2ee566dadad48ae110b15c
+ <+>
2022-06-09 20:31:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(name, names) {
+  return name.filter(
+    (expectedName) => expectedName.toLowerCase() === names.toLowerCase()
+  );
+}
+function fuzzyMatch(one, two) {
+  return one.filter((driversName) => driversName.indexOf(two) === 0);
+}
+function matchName(three, four) {
+  return three.filter((yes) => yes.name === four);
+}
